8e828ab47cae6e005154aa933fcb99d29494b9de
Dave Wilde <david.wilde@rackspace.com>
2017-08-30 22:25:51 -0500

    Removed debugging statements

--- isIPv4Address/isIPv4Address.go
+++ isIPv4Address/isIPv4Address.go
@@ -1,12 +1,10 @@
 package isIPv4Address
 
 import (
-	"fmt"
 	"regexp"
 )
 
 func isIPv4Address(inputString string) bool {
-	fmt.Printf("inputString: %v\n", inputString)
 	validatingIPv4regex := `^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$`
 	r := regexp.MustCompile(validatingIPv4regex)
 	return r.MatchString(inputString)
